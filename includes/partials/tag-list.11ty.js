0e9394b3d324f161c6b1bd1e3a81e2eaab51f550
Deniz Akşimşek <d.aaksimsek1@outlook.com>
2020-12-05 18:56:42 +0300

    convert entry page to 11ty.js & hyperscript

---
+++ includes/partials/tag-list.11ty.js
@@ -0,0 +1,19 @@
+
+const h = require('hyperscript')
+
+module.exports = function(tags, collections) {
+	return intersperse(',', 
+		tags.filter(tag => tag != 'all')
+			.map(tag =>
+				h('a.p-category', {href: `/archive/${tag}`},
+					tag, h('sup', collections[tag].length)
+				)
+			)
+		)
+}
+
+function intersperse(x, arr) {
+	return arr.reduce((acc, cur) =>
+		acc ? [cur] : (acc.push(x, cur), acc)
+	)
+}
