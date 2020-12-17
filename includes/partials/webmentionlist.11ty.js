d671d58a890bacca44351ea184c52407bbedc274
Deniz Akşimşek <deniz@denizaksimsek.com>
2020-12-18 01:41:43 +0300

    minor. fix,

--- includes/partials/webmentionlist.11ty.js
+++ includes/partials/webmentionlist.11ty.js
@@ -5,10 +5,10 @@ const webmention = require('./webmention.11ty.js')
 module.exports = function render(data) {
 	const { webmentions: { [data.page.url]: wms } } = data
 
-	if (!wms) return ''
+	if (!wms || wms.length < 1) return ''
 
 	return h('section.webmentions',
-		h('h2', wms.length, ' mentions'),
+		h('h2', `Mentions (${wms.length})`),
 		h('ol.webmentions-list', { reversed: 'reversed' },
 			wms.map(wm => h('li', webmention(data, wm)))
 		)
