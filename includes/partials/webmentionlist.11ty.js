48872aa29bb2bca94df0bf67a0c0bb9f8e181e8c
Deniz Akşimşek <39595683+DenizAksimsek@users.noreply.github.com>
2020-12-19 01:27:45 +0300

    ffs

--- includes/partials/webmentionlist.11ty.js
+++ includes/partials/webmentionlist.11ty.js
@@ -5,7 +5,7 @@ const { escape: encodeURIComponent } = require('querystring')
 
 const aboutWm = "https://indieweb.org/Webmention"
 
-function commentparade() {
+function commentparade(pageUrl) {
 	return h('a', { href: `https://quill.p3k.io/?dontask=1\
 &me=https://commentpara.de\
 &reply=https%3a%2f%2fwww.denizaksimsek.com%2f${encodeURIComponent(pageUrl)}` }, 'commentpara.de')
@@ -21,7 +21,7 @@ module.exports = function render(data) {
 	return h('section.webmentions',
 		h('h2', `Mentions (${wms.length})`),
 		h('p', 'This website accepts ', h('a', { href: aboutWm }, 'Webmentions'), '.',
-		       'Send one from your site, or anonymously from ', commentparade(), '.'),
+		       'Send one from your site, or anonymously from ', commentparade(data.page.url), '.'),
 		h('ol.webmentions-list', { reversed: 'reversed' },
 			wms.map(wm => h('li', webmention(data, wm)))
 		)
