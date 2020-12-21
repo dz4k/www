401c1c824557a3f9727dab0df40c35611eee9b64
Deniz Akşimşek <39595683+DenizAksimsek@users.noreply.github.com>
2020-12-21 15:20:16 +0300

    Update archive.tr.11ty.js

--- pages/archive.tr.11ty.js
+++ pages/archive.tr.11ty.js
@@ -1,7 +1,7 @@
 
 module.exports = class extends require('./archive.11ty.js') {
   data() {
-    return Object.assign(super(), {
+    return Object.assign(super.data(), {
       lang: 'tr',
       permalink: ({tag: etiket}) => etiket === 'all' ? '/arsiv/' : `/arsiv/${etiket}/`,
     }
