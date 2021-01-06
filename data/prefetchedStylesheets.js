f83ec87d512d262e488f7a7c1c4c03ea0c20e04f
Deniz Akşimşek <39595683+DenizAksimsek@users.noreply.github.com>
2021-01-06 17:44:13 +0300

    Update prefetchedStylesheets.js

--- data/prefetchedStylesheets.js
+++ data/prefetchedStylesheets.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 
 const urls = [
   // Prism syntax theme: VS
-  'https://unpkg.com/prism-themes@1.4.0/themes/prism-vs.css'
+  'https://unpkg.com/prism-themes@1.4.0/themes/prism-a11y-dark.css'
 ]
 
 module.exports = Promise.all(
