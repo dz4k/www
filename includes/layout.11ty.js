3bbdb974451a2e4ff54eccb0ca75fe6988cf5f37
Deniz Akşimşek <39595683+DenizAksimsek@users.noreply.github.com>
2021-01-10 23:01:22 +0300

    Update layout.11ty.js

--- includes/layout.11ty.js
+++ includes/layout.11ty.js
@@ -11,7 +11,12 @@ module.exports = class {
     const intl = data.intl.for(data.lang)
 
     return h('div',
-      h('header.site-header', h('strong', h('a.h-card', {href: '/'}, 'Deniz Akşimşek'))),
+      h('header.site-header',
+        h('strong', h('a.h-card', {href: '/'}, 'Deniz Akşimşek')),
+        h('img', { src: '/assets/me.qr.svg', style: {
+          float: 'right', width: '14ch', marginRight: '.5em'
+        } })
+       ),
       h('main', {innerHTML: content}),
       h('footer.site-footer',
         h('div', {innerHTML: intl.footer_message}),
