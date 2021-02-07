730895edde2aaeb8f2bf22991b20b60494f9ee49
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-02-07 18:24:34 +0300

    homepage minientry fix

--- posts/posts.11tydata.js
+++ posts/posts.11tydata.js
@@ -3,6 +3,11 @@ const { DateTime } = require('luxon')
 module.exports = {
 	layout: "entry-layout.njk",
 	eleventyComputed: {
-		permalink: ({page}) => `/${DateTime.fromJSDate(page.date).year}/${page.fileSlug}/`
+		permalink: ({page, title}) => {
+			const dt = DateTime.fromJSDate(page.date)
+			const year = dt.year
+			if (title) return `/${year}/${page.fileSlug}/`
+			else return `/${year}/${dt.toFormat('MMddHHmmss')}`
+		}
 	}
 }
