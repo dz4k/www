087267c77d0ecca492f248a2b749e09cffafddc5
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-02-07 19:01:42 +0300

    titling

--- posts/posts.11tydata.js
+++ posts/posts.11tydata.js
@@ -8,6 +8,10 @@ module.exports = {
 			const year = dt.year
 			if (title) return `/${year}/${page.fileSlug}/`
 			else return `/${year}/${dt.toFormat('MMddHHmmss')}/`
-		}
+		},
+		tagline: (d) => d.title ? d.title :
+			d.replyTo ? "Re." + (d.replyTo.name || d.replyTo) :
+			d.repostOf ? "Bookmark 🔖" + (d.repostOf.name || d.repostOf) :
+			d.likeOf ? "Like ❤" + (d.likeOf.name || d.likeOf) 
 	}
 }
