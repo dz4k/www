aec607ec65f9f519d04992b6a9140997e5368f50
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-02-07 19:27:56 +0300

    red hearts

--- posts/posts.11tydata.js
+++ posts/posts.11tydata.js
@@ -12,6 +12,6 @@ module.exports = {
 		tagline: (d) => d.title ? d.title :
 			d.replyTo ? "Re." + (d.replyTo.name || d.replyTo) :
 			d.repostOf ? "Bookmark 🔖" + (d.repostOf.name || d.repostOf) :
-			d.likeOf ? "Like ❤" + (d.likeOf.name || d.likeOf) 
+			d.likeOf ? "Like ❤️ " + (d.likeOf.name || d.likeOf) 
 	}
 }
