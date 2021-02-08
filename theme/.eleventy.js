70319ebc99f9182f0f5cd2a033014804f681c47d
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-02-09 01:51:50 +0300

    fix

--- theme/.eleventy.js
+++ theme/.eleventy.js
@@ -12,7 +12,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'))
 
   return {
-    dir: { output: '../_site', includes: 'includes', data: 'data' },
+    dir: { output: '../_site', includes: 'theme/includes', data: 'theme/data' },
   }
 }
 
