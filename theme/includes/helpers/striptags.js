b8887f936ff7d190ca4653abc03cbbeddea0c8c9
Deniz Akşimşek <deniz@denizaksimsek.com>
2021-02-09 18:19:18 +0300

    remove all .11ty.js

--- theme/includes/helpers/striptags.js
+++
@@ -1,6 +0,0 @@
-
-// // !!! ONLY USE WITH TRUSTED INPUT !!!
-// // https://gist.github.com/muhittin/3358070
-// module.exports = function striptags(text) {
-// 	return text.replace(/(<([^>]+)>)/ig,"")
-// }
